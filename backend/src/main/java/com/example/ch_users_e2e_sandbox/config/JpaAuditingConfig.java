21c0b455e6984c9d5a61c396f1eacb0d5748ef2c
Federico Bejarano <federicojuanbejarano@gmail.com>
2026-05-01 17:28:15 -0300

    habilitar auditing global

---
+++ backend/src/main/java/com/example/ch_users_e2e_sandbox/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.example.ch_users_e2e_sandbox.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
